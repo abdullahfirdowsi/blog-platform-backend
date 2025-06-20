f667a979d7388f9c562b4df828594fd9d38483e3
abdullahfirdowsi <abdullahtrickings@gmail.com>
2025-06-20 12:42:43 +0530

    fix: temporarily allow all CORS policy

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ app.add_exception_handler(RequestValidationError, validation_exception_handler)
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=["*"],  # Temporarily allowing all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
